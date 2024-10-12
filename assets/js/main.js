342134c9df39c119356aad66f020793e1fdf38ee
BiggiBoi <pervyshin@gmail.com>
2024-10-12 18:36:53 +0500

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
     let coin = document.querySelector('.coin');
 
-    coin.addEventListener('click', function(){
-        coin.classList.add('slide-fwd-center');
-        coin.addEventListener('animationend', function(){
-            coin.classList.remove('slide-fwd-center');
-        }, {once:true});
+    coin.addEventListener('click', (e){
+        let x = e.offsetX;
+        let y = e.offsetY;
+        navigator.vibrate(5);
+        if(x < 150 & y < 150){
+            coin.style.transform = 'translate(-0.25rem, -0.25rem) skewY(-10deg) skewX(5deg)';
+        }
+        else if (x < 150 & y > 150){
+            coin.style.transform = 'translate(-0.25rem, 0.25rem) skewY(-10deg) skewX(5deg)';
+        }
+        else if (x > 150 & y > 150){
+            coin.style.transform = 'translate(0.25rem, 0.25rem) skewY(10deg) skewX(-5deg)';
+        }
+        else if (x > 150 & y < 150){
+            coin.style.transform = 'translate(0.25rem, -0.25rem) skewY(10deg) skewX(-5deg)';
+        }
+
+        setTimeout(()=>{
+            coin.style.transform = 'translate(0px, 0px)';
+        }, 100);
+       // coin.classList.add('slide-fwd-center');
+       // coin.addEventListener('animationend', function(){
+       //     coin.classList.remove('slide-fwd-center');
+       // }, {once:true});
     });
 });
